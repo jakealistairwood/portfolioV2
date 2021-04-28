e91031603c678f6a9fae567571499b3a98a46cfc
Jake Wood <jakealistairwood@gmail.com>
2021-04-29 00:26:42 +0100

    projects are now rendered - majority of base styles in situ

--- src/components/Portfolio/Projects/Projects.jsx
+++ src/components/Portfolio/Projects/Projects.jsx
@@ -1,11 +1,44 @@
 import React from 'react';
 import styles from './Projects.module.scss';
 import { projects } from '../../../projects';
+import CodeIcon from '../../../assets/img/code-icon.svg';
+import EyeIcon from '../../../assets/img/eye-icon.svg';
 
 const Projects = () => {
+
+    console.log(projects);
+
     return (
         <div className={styles.projects}>
-            
+            {projects.map(project => (
+                <div className={styles.project}>
+                    <div className={styles.project__left}>
+                        <div className={styles.project__header}>
+                            <h3>{project.title}</h3>
+                            <p className={styles.project__description}>{project.description}</p>
+                        </div> 
+                        <div className={styles.project__footer}>
+                            <div className={styles.project__skills}>
+                                {project.skills.map((skill) => (
+                                    <div className={styles.skillContainer}>
+                                        <span className={styles.skill}>{skill}</span>
+                                    </div>
+                                ))}
+                            </div>
+                            <div className={styles.project__links}>
+                                <a href={project.preview} target="_blank">
+                                    <img src={EyeIcon} alt="preview-project-icon" />
+                                    <span>Preview</span>
+                                </a>
+                                <a href={project.code} target="_blank">
+                                    <img src={CodeIcon} alt="preview-codebase-icon" />
+                                    <span>Code</span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>    
+                </div>
+            ))}
         </div>
     )
 }
--- src/projects.js
+++ src/projects.js
@@ -3,7 +3,7 @@ export const projects = [
         title: 'Devjobs',
         description: 'Recreated a frontendmentor challenge using Figma and React JS. The Github Jobs API was used to render the job adverts with features including: <br> - Pagination to limit number of cards on the page. <br> - Light/Dark mode using styled-components',
         skills: ['React', 'Styled Components', 'Axios', 'Postman'],
-        img: 'img/previews/devjobs-preview.svg',
+        img: '/img/previews/devjobs-preview.svg',
         code: 'https://github.com/jakealistairwood/github-jobs-api',
         preview: 'https://github-jobs-api-e3da5.web.app/'
     },
@@ -11,7 +11,7 @@ export const projects = [
         title: 'Small Margins',
         description: 'Recreated a frontendmentor challenge using Figma and React JS. The Github Jobs API was used to render the job adverts with features including: <br> - Pagination to limit number of cards on the page. <br> Light/Dark mode using styled-components',
         skills: ['Express.js', 'Node.js', 'CRUD', 'Postman'],
-        img: 'img/previews/smallmargins-preview.svg',
+        img: '/img/previews/smallmargins-preview.svg',
         code: 'https://github.com/jakealistairwood/small-margins',
         preview: 'https://mighty-bayou-76861.herokuapp.com/'
     },
@@ -19,7 +19,7 @@ export const projects = [
         title: 'Brewdog API',
         description: 'A React App that uses an API to fetch data and display it on the application. A user can search and filter their beer selection by a range of properties.',
         skills: ['React', 'API queries', 'SCSS'],
-        img: 'img/previews/brewdog-preview.svg',
+        img: '/img/previews/brewdog-preview.svg',
         code: 'https://github.com/jakealistairwood/brewdog-api',
         preview: 'https://brewdogapi-b5d03.web.app'
     },
@@ -27,7 +27,7 @@ export const projects = [
         title: 'Mariana Portfolio',
         description: 'Asked by the _nology team to personally design and help develop the new website to showcase the portfolios of the intake I was a part off. The website was built with React and designed in Figma.',
         skills: ['React', 'Figma', 'SCSS'],
-        img: 'img/previews/mariana-preview.svg',
+        img: '/img/previews/mariana-preview.svg',
         code: 'https://github.com/jakealistairwood/mariana-portfolio',
         preview: 'https://mariana.nology.io/'
     },
@@ -35,7 +35,7 @@ export const projects = [
         title: 'JS Quiz Game',
         description: 'A Kahoot inspired quiz-game developed using Vanilla JS. Currently a work in progress while I fix responsiveness and render more questions onto page!',
         skills: ['Vanilla JS', 'Functional programming', 'OOP'],
-        img: 'img/previews/js-quiz-preview.svg',
+        img: '/img/previews/js-quiz-preview.svg',
         code: 'https://github.com/jakealistairwood/js-challenge-quiz-game',
         preview: 'https://jakealistairwood.github.io/js-challenge-quiz-game/'
     },
@@ -43,7 +43,7 @@ export const projects = [
         title: 'JS Calculator',
         description: 'The first JS challenge as part of the _nology course in which I was tasked to produce a simple calculator using vanilla JS.',
         skills: ['Vanilla JS', 'Functional programming', 'SCSS'],
-        img: 'img/previews/js-calculator-preview.svg',
+        img: '/img/previews/js-calculator-preview.svg',
         code: 'https://github.com/jakealistairwood/js-calculator',
         preview: 'https://jakealistairwood.github.io/js-calculator/'
     },
