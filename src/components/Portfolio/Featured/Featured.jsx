61a7abda216a88eb730849b1e6ba81f7e5f168e9
Jake Wood <jakealistairwood@gmail.com>
2021-05-10 13:31:14 +0100

    switching dada preview and code link

--- src/components/Portfolio/Featured/Featured.jsx
+++ src/components/Portfolio/Featured/Featured.jsx
@@ -21,11 +21,11 @@ const Featured = () => {
                         ))}
                     </div>
                     <div className={styles.featured__footerRight}>
-                        <a href={featured.code} target="_blank" rel="noreferrer">
+                        <a href={featured.preview} target="_blank" rel="noreferrer">
                             <img src={EyeIcon} alt="preview-project-icon" />
                             <span>Preview</span>
                         </a>
-                        <a href={featured.preview} target="_blank" rel="noreferrer">
+                        <a href={featured.code} target="_blank" rel="noreferrer">
                             <img src={CodeIcon} alt="preview-codebase-icon" />
                             <span>Code</span>
                         </a>
